517b8f3c0ef8d2725c0d9b1cab1ae3e393ed10f1
João Abreu <abreunegocio@gmail.com>
2023-07-26 14:58:01 -0300

    ajuste dos campos de acordo com back

--- FrontEnd/apexv13/src/app/clientes/edit-clientes/edit-clientes.component.ts
+++ FrontEnd/apexv13/src/app/clientes/edit-clientes/edit-clientes.component.ts
@@ -26,40 +26,39 @@ export class EditClientesComponent implements OnInit {
 
   ngOnInit(): void {
     this.formVeiculo = this.fb.group({
-      marcaVeiculo: [null],
-      placaVeiculo: [null],
-      corVeiculo: [null],
-      anoVeiculo: [null],
-      modeloVeiculo: [null],
-      kmVeiculo: [null],
-
+      marca: [null],
+      placa: [null],
+      cor: [null],
+      ano: [null],
+      modelo: [null],
+      km: [null],
     });
 
     this.formCliente = this.fb.group({
-      nomeCliente: [null],
-      cpfCliente: [null],
+      nome: [null],
+      cpf: [null],
       dataNascimento: [null],
       endereco: [null],
       bairro: [null],
       cidade: [null],
       uf: [null],
       numeroWhatsapp: [null],
-      telefoneContato: [null],
+      numeroContato: [null],
       email: [null],
     });
   }
 
   onSubmitVeiculos() {
     const veiculos = new Veiculos();
-    veiculos.anoVeiculo = this.formVeiculo.value.anoVeiculo;
-    veiculos.corVeiculo = this.formVeiculo.value.corVeiculo;
-    veiculos.kmVeiculo = this.formVeiculo.value.kmVeiculo;
-    veiculos.marcaVeiculo = this.formVeiculo.value.marcaVeiculo;
-    veiculos.modeloVeiculo = this.formVeiculo.value.modeloVeiculo;
-    veiculos.placaVeiculo = this.formVeiculo.value.placaVeiculo;
+    veiculos.ano = this.formVeiculo.value.ano;
+    veiculos.cor = this.formVeiculo.value.cor;
+    veiculos.km = this.formVeiculo.value.km;
+    veiculos.marca = this.formVeiculo.value.marca;
+    veiculos.modelo = this.formVeiculo.value.modelo;
+    veiculos.placa = this.formVeiculo.value.placa;
 
     console.log(veiculos);
-    this.clientes.veiculos.push(veiculos);
+    this.clientes.automoveis.push(veiculos);
     console.warn(this.formVeiculo.value);
     this.formVeiculo.reset();
 
@@ -67,79 +66,65 @@ export class EditClientesComponent implements OnInit {
   }
 
   deleteVeiculo(index: number) {
-    this.clientes.veiculos.splice(index, 1);
+    this.clientes.automoveis.splice(index, 1);
   }
 
   editVeiculo(index: number) {
     this.formVeiculo = this.fb.group({
-      anoVeiculo: this.clientes.veiculos[index].anoVeiculo,
-      corVeiculo: this.clientes.veiculos[index].corVeiculo,
-      kmVeiculo: this.clientes.veiculos[index].kmVeiculo,
-      marcaVeiculo: this.clientes.veiculos[index].marcaVeiculo,
-      modeloVeiculo: this.clientes.veiculos[index].modeloVeiculo,
-      placaVeiculo: this.clientes.veiculos[index].placaVeiculo,
+      ano: this.clientes.automoveis[index].ano,
+      cor: this.clientes.automoveis[index].cor,
+      km: this.clientes.automoveis[index].km,
+      marca: this.clientes.automoveis[index].marca,
+      modelo: this.clientes.automoveis[index].modelo,
+      placa: this.clientes.automoveis[index].placa,
     });
 
-    this.clientes.veiculos.splice(index, 1);
+    this.clientes.automoveis.splice(index, 1);
   }
 
   onSave() {
-    if (this.formCliente.valid && this.formCliente.touched) {
-      this.clientes.nomeCliente = this.formCliente.value.nomeCliente;
-      this.clientes.cpfCliente = this.formCliente.value.cpfCliente;
+
+      this.clientes.nome = this.formCliente.value.nome;
+      this.clientes.cpf = this.formCliente.value.cpf;
       this.clientes.dataNascimento = this.formCliente.value.dataNascimento;
       this.clientes.endereco = this.formCliente.value.endereco;
       this.clientes.bairro = this.formCliente.value.bairro;
       this.clientes.cidade = this.formCliente.value.cidade;
       this.clientes.uf = this.formCliente.value.uf;
       this.clientes.numeroWhatsapp = this.formCliente.value.numeroWhatsapp;
-      this.clientes.telefoneContato = this.formCliente.value.telefoneContato;
+      this.clientes.numeroContato = this.formCliente.value.numeroContato;
       this.clientes.email = this.formCliente.value.email;
 
-      const veiculos = this.clientes.veiculos.slice();
+this.clienteService.createClient(this.clientes).subscribe((data)=> {
+  console.log(data)
+})
 
-      const dadosParaSalvar = {
-        veiculos: veiculos,
-        cliente: {
-          nomeCliente: this.formCliente.value.nomeCliente,
-          cpfCliente: this.formCliente.value.cpfCliente,
-          dataNascimento: this.formCliente.value.dataNascimento,
-          endereco: this.formCliente.value.endereco,
-          bairro: this.formCliente.value.bairro,
-          cidade: this.formCliente.value.cidade,
-          uf: this.formCliente.value.uf,
-          numeroWhatsapp: this.formCliente.value.numeroWhatsapp,
-          telefoneContato: this.formCliente.value.telefoneContato,
-          email: this.formCliente.value.email,
-        },
-      };
+      console.log("DADOS PARA SALVAR:", this.clientes);
 
-      console.log("DADOS PARA SALVAR:", dadosParaSalvar);
+    //   console.log("CLIENTES", this.clientes);
+    //   const result$ = this.alertService.showConfirm(
+    //     "Confirmação",
+    //     "Deseja confirmar o agendamento?"
+    //   );
+    //   result$
+    //   .asObservable()
+    //   .pipe(
+    //     take(1),
+    //     switchMap((result) =>
+    //       result ? this.clienteService.createClient(this.clientes) : EMPTY
+    //     )
+    //   )
+    //   .subscribe(
+    //     (clientes) => console.log(clientes),
+    //     (error) => console.error(error)
+    //   );
 
-      console.log("CLIENTES", this.clientes);
-      const result$ = this.alertService.showConfirm(
-        "Confirmação",
-        "Deseja confirmar o agendamento?"
-      );
-      result$
-      .asObservable()
-      .pipe(
-        take(1),
-        switchMap((result) =>
-          result ? this.clienteService.createClient(dadosParaSalvar) : EMPTY
-        )
-      )
-      .subscribe(
-        (clientes) => console.log(clientes),
-        (error) => console.error(error)
-      );
-
-    this.router.navigate(["clientes"]);
-  } else {
-    this.alertService.showAlertDanger(
-      "Preencha todos os campos obrigatórios."
-    );
-  }
+    // this.router.navigate(["clientes"]);
+  // } else {
+  //   this.alertService.showAlertDanger(
+  //     "Preencha todos os campos obrigatórios."
+  //   );
+  // }
   }
 
   goBack() {
