cbcdf37a67d26f4fca7da594722fb541f77d698c
matheus <matheus.bueno@youxgroup.com.br>
2023-08-17 16:19:12 -0300

    EnvioEmail

--- Backend/Aplicattion/Service/ManutenceService.cs
+++ Backend/Aplicattion/Service/ManutenceService.cs
@@ -1,7 +1,11 @@
 ﻿using GestaoOfficina.Domain.Model;
+using GestaoOfficina.Infra.Interface;
 using GestaoOfficinaProj.Domain.DTO;
 using GestaoOfficinaProj.Domain.Model;
 using GestaoOfficinaProj.Infra.Interface;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,15 +17,19 @@ namespace GestaoOfficinaProj.Aplicattion.Service
     public class ManutenceService : IManutenceService
     {
         private readonly IManutenceRepository _manutenceRepository;
-        public ManutenceService(IManutenceRepository manutenceRepository)
+        private readonly IClientRepository _clientRepository;
+        public ManutenceService(IManutenceRepository manutenceRepository, IClientRepository clientRepository)
         {
             _manutenceRepository = manutenceRepository;
+            _clientRepository = clientRepository;
         }
         public async Task<ReturnDefault> Create(ManutenceCreateDTO entrada)
         {
+
+            #region criação manutence
             ManutenceServico objeto = new ManutenceServico();
             Manutence objetoPai = new Manutence();
-
+            
             objetoPai.AutomovelId = entrada.Veiculoid;
             objetoPai.ClientId = entrada.Clientid;
             objetoPai.Observacoes = entrada.Observacoes;
@@ -31,6 +39,82 @@ namespace GestaoOfficinaProj.Aplicattion.Service
             objetoPai.ManutecesServicos = entrada.manutences;
             objetoPai.ValorTotal = entrada.ValorTotal;
             _manutenceRepository.Create(objetoPai);
+
+            var result = _clientRepository.GetClientById(objetoPai.ClientId);
+            #endregion
+
+            #region corpo email
+            // Informações da oficina
+            string nomeOficina = "Oficina ABC";
+            string enderecoOficina = "Rua Principal, 123";
+            string cidadeOficina = "Cidade da Oficina";
+            string telefoneOficina = "(123) 456-7890";
+
+            // Informações do cliente
+            string nomeCliente = result.Result.Nome;
+            string enderecoCliente = result.Result.Endereco;
+            string cidadeCliente = result.Result.Cidade;
+            string telefoneCliente = result.Result.NumeroContato;
+
+            // Itens de serviço
+            List<string> servicos = new List<string>();
+            foreach (var item in entrada.manutences)
+            {
+                servicos.Add(item.Nome);
+                
+            }
+            
+
+            // Montar a nota de serviço em uma string
+            string notaDeServico = $"======== NOTA DE SERVIÇO ========{Environment.NewLine}" +
+                                   $"Oficina: {nomeOficina}{Environment.NewLine}" +
+                                   $"Endereço: {enderecoOficina}, {cidadeOficina}{Environment.NewLine}" +
+                                   $"Telefone: {telefoneOficina}{Environment.NewLine}" +
+                                   $"--------------------------------{Environment.NewLine}" +
+                                   $"Cliente: {nomeCliente}{Environment.NewLine}" +
+                                   $"Endereço: {enderecoCliente}, {cidadeCliente}{Environment.NewLine}" +
+                                   $"Telefone: {telefoneCliente}{Environment.NewLine}" +
+                                   $"--------------------------------{Environment.NewLine}" +
+                                   $"Serviços realizados:{Environment.NewLine}";
+
+            foreach (string servico in servicos)
+            {
+                notaDeServico += $"- {servico}{Environment.NewLine}";
+            }
+
+            notaDeServico += $"--------------------------------{Environment.NewLine}" +
+                             $"Total a pagar: R$ 500,00{Environment.NewLine}" +
+                             $"================================";
+            #endregion
+
+            #region envio email
+
+            string remetenteEmail = "bueno.mb55@hotmail.com";
+            string senhaRemetente = "Aabbcc12!";
+            string destinatarioEmail = result.Result.Email;
+            string assunto = "Assunto do Email";
+            string corpo = notaDeServico;
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Remetente", remetenteEmail));
+            message.To.Add(new MailboxAddress("Destinatário", destinatarioEmail.ToString()));
+            message.Subject = assunto;
+            message.Body = new TextPart("plain")
+            {
+                Text = corpo
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true; // Ignorar validação do certificado
+                await client.ConnectAsync("smtp-mail.outlook.com", 587, SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync(remetenteEmail, senhaRemetente);
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+            }
+
+            #endregion
+
             return new ReturnDefault("Dados retornado com sucesso.", objeto);
         }
 
