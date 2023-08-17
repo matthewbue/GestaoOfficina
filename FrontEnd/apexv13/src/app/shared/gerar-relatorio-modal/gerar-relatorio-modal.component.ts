565bdfc2bb063ab107ec34e729b35971737f46b0
João Abreu <abreunegocio@gmail.com>
2023-08-17 17:49:30 -0300

    Ajuste relatorio

--- FrontEnd/apexv13/src/app/shared/gerar-relatorio-modal/gerar-relatorio-modal.component.ts
+++ FrontEnd/apexv13/src/app/shared/gerar-relatorio-modal/gerar-relatorio-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { OrdemdeServicoService } from 'app/ordemdeservico/ordemdeservico.service';
 import jsPDF from 'jspdf';
+import { AlertModalService } from '../services/alert-modal.service';
 
 
 @Component({
@@ -18,7 +19,8 @@ export class GerarRelatorioModalComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private osService: OrdemdeServicoService
+    private osService: OrdemdeServicoService,
+    private alertService: AlertModalService,
   ) { }
 
   ngOnInit(): void {
@@ -43,39 +45,32 @@ export class GerarRelatorioModalComponent implements OnInit {
   }
 
   baixarRelatorio() {
-    if (this.formRelatorio.value.dataInicial != "" && this.formRelatorio.value.dataFinal != "") {
-
-      const requestRelatorio = {
+    const requestRelatorio = {
         nomeCliente: this.formRelatorio.value.nomeCliente == null ? "" : this.formRelatorio.value.nomeCliente,
         statusOs: this.statusSelected == null ? "" : this.statusSelected,
         tipoDoc: this.tipoSelected == null ? "" : this.tipoSelected,
-        dataInicial: this.formRelatorio.value.dataInicial == null ? new Date : this.formRelatorio.value.dataInicial,
-        dataFinal: this.formRelatorio.value.dataFinal == null ? new Date : this.formRelatorio.value.dataFinal,
-      }
-      console.log(requestRelatorio)
-      this.osService.getRelatorioFilter(requestRelatorio).subscribe((response) => {
-        console.log(response)
+        dataInicial: this.formRelatorio.value.dataInicial == null ? "" : this.formRelatorio.value.dataInicial,
+        dataFinal: this.formRelatorio.value.dataFinal == null ? "" : this.formRelatorio.value.dataFinal,
+    }
 
+    if (requestRelatorio.dataInicial == "" || requestRelatorio.dataFinal == "") {
+        console.log("Escolha data Inicial e data Final para gerar o relatório!")
+        this.alertService.showAlertDanger("Escolha data Inicial e data Final para gerar o relatório!");
+        return;
+    }
+
+    this.osService.getRelatorioFilter(requestRelatorio).subscribe((response) => {
         const doc = new jsPDF();
-
-        // // Adicione a imagem como marca d'água
-        // const imgData = '../../../assets/img/logo-oficina-peb.png';
-        // const imgWidth = 150; // Largura da imagem (ajuste conforme necessário)
-        // const imgHeight = (imgWidth * 1.41); // Proporção de aspecto da imagem (ajuste conforme necessário)
-        // const xPos = (doc.internal.pageSize.getWidth() - imgWidth) / 2; // Posição horizontal central
-        // const yPos = (doc.internal.pageSize.getHeight() - imgHeight) / 2; // Posição vertical central
-        // doc.addImage(imgData, 'PNG', xPos, yPos, imgWidth, imgHeight, '', 'FAST', 0.1); // Ajuste o valor de transparência (0.1 neste caso)
-
-        doc.setFont('courier', 'normal'); // Defina a fonte padrão
-        doc.setTextColor(0, 0, 0); // Defina a cor do texto (preto)
+        doc.setFont('courier', 'normal');
+        doc.setTextColor(0, 0, 0);
 
         doc.setFontSize(18);
         doc.setFont('courier', 'bold');
         doc.text('FERREIRA\'S AUTOMOTIVO', 105, 20, { align: 'center' });
-        doc.setFont('courier', 'normal'); // Voltar à fonte normal
+        doc.setFont('courier', 'normal');
 
         doc.setFontSize(12);
-        doc.text('CNPJ: 20.388.818/0001-30', 105, 30, { align: 'center' }); // Adicione o campo CNPJ aqui
+        doc.text('CNPJ: 20.388.818/0001-30', 105, 30, { align: 'center' });
 
         doc.setFontSize(12);
         doc.text('Rua Framboesa LOTE 1 QUADRA S - 23061-522 - (21)964169157', 105, 40, { align: 'center' });
@@ -83,96 +78,87 @@ export class GerarRelatorioModalComponent implements OnInit {
         doc.setFontSize(14);
         doc.setFont('courier', 'bold');
         doc.text('RELATÓRIO', 105, 55, { align: 'center' });
-        doc.setFont('courier', 'normal'); // Voltar à fonte normal
+        doc.setFont('courier', 'normal');
 
         let yPosValue = 70;
+        let currentPage = 1;
 
-        doc.setFontSize(12);
-        doc.text(`Número da Ordem de Serviço: ${"this.ordemServico.id"}`, 20, yPosValue);
-        yPosValue += 10;
-        doc.text(`Data: ${"this.ordemServico.dataOS"}`, 20, yPosValue);
-        yPosValue += 10;
+        response.data.manutences.forEach((manutence, index) => {
+            if (index > 0 && index % 2 === 0) {
+                doc.addPage(); // Adicionar uma nova página a cada dois registros
+                currentPage++;
+                yPosValue = 40;
+            }
 
-        doc.text(`Cliente: ${"this.cliente.nome"}`, 20, yPosValue);
-        doc.text(`CPF: ${"this.cliente.cpf"}`, 105, yPosValue);
-        yPosValue += 10;
+            doc.setFontSize(12);
+            doc.text(`Número da Ordem de Serviço: ${manutence.id}`, 20, yPosValue);
+            yPosValue += 10;
+            doc.text(`Data: ${manutence.dataOS}`, 20, yPosValue);
+            yPosValue += 10;
 
-        doc.text(`Endereço: ${"this.cliente.endereco"}`, 20, yPosValue);
-        yPosValue += 15;
+            doc.text(`Cliente: ${manutence.clients.nome}`, 20, yPosValue);
+            doc.text(`CPF: ${manutence.clients.cpf}`, 105, yPosValue);
+            yPosValue += 10;
+
+            doc.text(`Endereço: ${manutence.clients.endereco}`, 20, yPosValue);
+            yPosValue += 15;
+
+            doc.setFontSize(14);
+            doc.setFont('courier', 'bold');
+            doc.text('Informações do Veículo', 105, yPosValue, { align: 'center' });
+            doc.setFont('courier', 'normal');
+            yPosValue += 10;
+
+            doc.setFontSize(12);
+            doc.text(`Placa: ${manutence.automovels.placa}`, 20, yPosValue);
+            doc.text(`Marca: ${manutence.automovels.marca}`, 80, yPosValue);
+            doc.text(`Modelo: ${manutence.automovels.modelo}`, 140, yPosValue);
+            yPosValue += 10;
+            doc.text(`Ano: ${manutence.automovels.ano}`, 20, yPosValue);
+            doc.text(`Cor: ${manutence.automovels.cor}`, 80, yPosValue);
+            doc.text(`Km Atual: ${manutence.automovels.km}`, 140, yPosValue);
+            yPosValue += 20;
+
+            doc.setFontSize(14);
+            doc.setFont('courier', 'bold');
+            doc.text('Serviços Realizados', 105, yPosValue, { align: 'center' });
+            doc.setFont('courier', 'normal');
+            yPosValue += 10;
+
+            doc.setFontSize(12);
+            if (manutence.manutecesServicos) {
+                const servicosFeitos = manutence.manutecesServicos.map(servico => `- ${servico.nome}: R$ ${servico.valor},00`).join('\n');
+                doc.text(servicosFeitos, 20, yPosValue);
+                yPosValue += doc.splitTextToSize(servicosFeitos, 160).length * 10 + 10;
+            }
+
+            doc.setFontSize(14);
+            doc.text(`Observações: ${manutence.observacoes}`, 105, yPosValue, { align: 'center' });
+            yPosValue += 20;
+
+            doc.setFontSize(14);
+            doc.text(`Valor Total: R$ ${manutence.valorTotal},00`, 105, yPosValue, { align: 'center' });
+            yPosValue += 15;
+
+            if (index < response.data.manutences.length - 1) {
+                yPosValue += 20; // Espaço entre os registros
+            }
+
+            if (index === response.data.manutences.length - 1 && currentPage !== Math.ceil(response.data.manutences.length / 2)) {
+                doc.addPage(); // Adicionar uma nova página para o último registro
+                currentPage++;
+                yPosValue = 40;
+            }
+        });
 
         doc.setFontSize(14);
-        doc.setFont('courier', 'bold');
-        doc.text('Informações do Veículo', 105, yPosValue, { align: 'center' });
-        doc.setFont('courier', 'normal'); // Voltar à fonte normal
-
-        yPosValue += 10;
-        doc.setFontSize(12);
-        doc.text(`Placa: ${"this.automovel.placa"}`, 20, yPosValue);
-        doc.text(`Marca: ${"this.automovel.marca"}`, 80, yPosValue);
-        doc.text(`Modelo: ${"this.automovel.modelo"}`, 140, yPosValue);
-        yPosValue += 10;
-        doc.text(`Ano: ${"this.automovel.ano"}`, 20, yPosValue);
-        doc.text(`Cor: ${"this.automovel.cor"}`, 80, yPosValue);
-        doc.text(`Km Atual: ${"this.automovel.km"}`, 140, yPosValue);
-        yPosValue += 15;
-
-        doc.setFontSize(14);
-        doc.setFont('courier', 'bold');
-        doc.text('Serviços Realizados', 105, yPosValue, { align: 'center' });
-        doc.setFont('courier', 'normal'); // Voltar à fonte normal
-        yPosValue += 10;
-
-        doc.setFontSize(12);
-        // const servicosFeitos = this.manutencesServico.map(servico => `- ${servico.nome}: R$ ${servico.valor},00`).join('\n');
-        // doc.text(servicosFeitos, 20, yPosValue);
-        yPosValue += 10;
-
-        doc.setFontSize(14);
-        doc.setFont('courier', 'bold');
-        doc.text('Observações', 105, yPosValue, { align: 'center' });
-        doc.setFont('courier', 'normal'); // Voltar à fonte normal
-
-        yPosValue += 10;
-
-        doc.setFontSize(12);
-        doc.text(`${"this.ordemServico.observacoes"}`, 20, yPosValue);
-        yPosValue += 10;
-
-        doc.setFontSize(14);
-        doc.text(`Valor Total: R$ ${"this.ordemServico.valorTotal"},00`, 105, yPosValue + 10, { align: 'center' });
-
-        // Adicione campos de assinatura
-        yPosValue += 50; // Espaço entre o texto e as assinaturas
-
-        doc.setFontSize(12);
-        const assinaturaClienteY = yPosValue;
-        doc.text('Assinatura do Cliente:', 20, assinaturaClienteY);
-        doc.line(20, assinaturaClienteY + 10, 100, assinaturaClienteY + 10); // Linha para assinatura do cliente
-
-        const assinaturaResponsavelY = yPosValue;
-        doc.text('Assinatura do Responsável:', 105, assinaturaResponsavelY);
-        doc.line(105, assinaturaResponsavelY + 10, 200, assinaturaResponsavelY + 10); // Linha para assinatura do responsável
-
-        doc.setFontSize(10);
-        doc.text('Agradecemos pela preferência!', 105, doc.internal.pageSize.getHeight() - 15, { align: 'center' });
-
-        // Adicione a frase adicional
-        doc.setFontSize(10);
-        doc.text('Nunca foi sorte, sempre foi Deus!', 105, doc.internal.pageSize.getHeight() - 10, { align: 'center' });
+        doc.text(`Valor Total Geral: R$ ${response.data.valorTotalRelatorio},00`, 105, doc.internal.pageSize.getHeight() - 15, { align: 'center' });
 
         doc.save('relatorio.pdf');
-
-      })
-
-    }
-    else {
-      console.log("Preecha uma data válida")
-    }
+    });
+}
 
 
-
-
-  }
 
   limparFiltro() {
     this.formRelatorio.reset()
