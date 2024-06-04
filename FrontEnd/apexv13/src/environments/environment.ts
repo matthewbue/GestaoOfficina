a079ed19964c1eb7816e677a1fb847fa5772177f
joaoabreu22 <abreunegocio2@gmail.com>
2024-06-04 19:18:35 -0300

    env

--- FrontEnd/apexv13/src/environments/environment.ts
+++ FrontEnd/apexv13/src/environments/environment.ts
@@ -4,6 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  API: 'https://deploy-back-officinaferreira.azurewebsites.net',
+  // API: 'https://deploy-back-officinaferreira.azurewebsites.net',
+  API: 'https://localhost:44392',
   production: false
 };
